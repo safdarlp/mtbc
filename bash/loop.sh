5e678e4345e47ff6b62e1f3e82ad15a74bc7d7f2
SAFDAR KHAN <SAFDARLP@GMAIL.COM>
2021-05-24 10:15:06 -0500

    Bash exercise 5 - loop example

--- bash/loop.sh
+++ bash/loop.sh
@@ -8,3 +8,10 @@ do
   echo $COMMAND
 done
 
+# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+for FILENAME in $VHOSTS_PATH
+do
+  echo $FILENAME
+done
